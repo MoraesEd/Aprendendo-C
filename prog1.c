5cb9820d5b26d9f5f903fae168d19b056f49d8e8
EdMoraess <64334227+MoraesEd@users.noreply.github.com>
2022-09-26 12:05:56 -0300

    First codes

---
+++ prog1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+    printf("Hello World!\n");
+    printf("Lorem Ipsum");
+    /* Isso é um comentário*/
+    int i; /* Criação da variável i */
+    i = 7  /* Atribuição de um valor */
+    int b;
+    printf("Insira um número:");
+    scanf("%d", &b);
+    printf("O número inserido foi: %d");
+}
+
